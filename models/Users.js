a81b90f3002752849cf803c0c5834415addfdd65
Fábio Costa <fabiodanielmc@gmail.com>
2020-04-09 17:51:21 +0200

    login now works and redirects correctly

--- models/Users.js
+++ models/Users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 // const ObjectId = Schema.ObjectId;
- 
+
 const userSchema = new Schema({
   passWord: String,
   email: String,
