f9a2c17ffd4eeaa562cc835638edc1c5db5db6f6
unknown <damianosmakris@hotmail.com>
2020-04-10 16:57:25 +0200

    yeah

--- routes/user/profile.js
+++ routes/user/profile.js
@@ -1,18 +1,40 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const User = require("../../models/Users");
-const session    = require("express-session");
+const session = require("express-session");
+const multer = require('multer');
+const Picture = require('../../models/picture');
+const upload = multer({ dest: './public/uploads/' });
+const uploadCloud = require('../../config/cloudinary.js');
 
-router.get('/profile', (req, res, next)=>{
+router.get('/profile', (req, res, next) => {
     User
         .findById(req.session.currentUser._id)
-        .then((user)=>{
+        .then((user) => {
             res.json({
                 firstName: user.firstName,
                 email: user.email
             })
         })
-})
+    Picture.find((err, pictures) => {
+        res.json('index', { pictures })
+    })
+
+});
 
 
+
+router.post('/profile', uploadCloud.single('photo'), (req, res, next) => {
+    const { title, description } = req.body;
+    const imgPath = req.file.url;
+    const imgName = req.file.originalname;
+    const newphoto = new Picture({ title, description, imgPath, imgName })
+    newphoto.save()
+        .then(photo => {
+            res.redirect('/');
+        })
+        .catch(error => {
+            console.log(error);
+        })
+});
 module.exports = router;
