c50bbdd42cc16fa24cb39f3eda4d8497aa4a710f
unknown <damianosmakris@hotmail.com>
2020-04-15 20:07:01 +0200

    minor

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,7 @@ const uploadCloud = require('../config/cloudinary.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.json({name: "Fábio"});
+  res.json({name: "Hey"});
 });
 
-router.post('/movie/add', uploadCloud.single('photo'), (req, res, next) => {
-  const { title, description } = req.body;
-  const imgPath = req.file.url;
-  const imgName = req.file.originalname;
-  const newMovie = new Movie({title, description, imgPath, imgName})
-  newMovie.save()
-  .then(movie => {
-    res.redirect('/');
-  })
-  .catch(error => {
-    console.log(error);
-  })
-});
 module.exports = router;
